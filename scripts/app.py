933649c58706abf2960f236171d5f6fc73063cad
leck211 <157505373+leck211@users.noreply.github.com>
2024-06-06 19:39:28 +0300

    Add files via upload

--- scripts/app.py
+++ scripts/app.py
@@ -1,8 +1,37 @@
+import pygame
+import os
+from scripts.game import Game
 class App:
-
-
     def __init__(self):
+        self.fps = 60
+        self.scena = pygame.display.set_mode((480, 720))
+        self.time = pygame.time.Clock()
+        self.running = True
+        pygame.display.set_caption('DudleJump')
+        image = pygame.image.load(os.path.join('assets','icons','icon.ico'))
+        pygame.display.set_icon(image)
+        self.game = Game()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+    def update(self):
         ...
+    def render(self):
+        self.scena.fill((0, 0, 0))
+        self.game.render_objects(self.scena)
+        pygame.display.update()
     
+
+
+
     def run(self):
-        ...
+        while self.running:
+            self.handle_events()
+            self.update()
+            self.render()
+
+            self.time.tick(self.fps)
+
+    
---
+++ scripts/game.py
@@ -0,0 +1,8 @@
+import pygame
+import os
+class Game:
+    def __init__(self):
+        
+        self.background = pygame.image.load(os.path.join('assets','images','background.png'))
+    def render_objects(self, scena):
+        scena.blit(self.background, (0, 0))
