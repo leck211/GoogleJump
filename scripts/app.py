9a455f5a369c34f1204e3f7561a50b1470834250
leck211 <157505373+leck211@users.noreply.github.com>
2024-06-13 19:43:24 +0300

    Add files via upload

--- scripts/app.py
+++ scripts/app.py
@@ -1,23 +1,30 @@
 import pygame
-import os
 from scripts.game import Game
+from scripts.functions import load_image
 class App:
     def __init__(self):
         self.fps = 60
         self.scena = pygame.display.set_mode((480, 720))
         self.time = pygame.time.Clock()
         self.running = True
+
         pygame.display.set_caption('DudleJump')
-        image = pygame.image.load(os.path.join('assets','icons','icon.ico'))
-        pygame.display.set_icon(image)
+        pygame.display.set_icon(load_image('assets','icons','icon.ico'))
+
         self.game = Game()
 
     def handle_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+            
+            elif event.type == pygame.KEYDOWN:
+                self.game.process_key_down_event(event.key)
+            elif event.type == pygame.KEYUP:
+                self.game.process_key_up_event(event.key)
     def update(self):
-        ...
+        self.game.update_objects()
+
     def render(self):
         self.scena.fill((0, 0, 0))
         self.game.render_objects(self.scena)
--- scripts/game.py
+++ scripts/game.py
@@ -1,8 +1,40 @@
+
+from scripts.sprite import Sprite
 import pygame
-import os
+from scripts.functions import load_image
+from scripts.player import Player
 class Game:
     def __init__(self):
-        
-        self.background = pygame.image.load(os.path.join('assets','images','background.png'))
+        self.background = load_image('assets','images','background.png')
+        self.platforms = [
+            Sprite(load_image('assets','images','background.png'))
+        ]
+        self.player = Player(
+            load_image('assets','images','player.png'),
+            (240, 600),
+            5,
+            15,
+            0.75,
+        )
+    def process_key_down_event(self, key):
+        if key == pygame.K_a:
+            self.player.is_walking_left = True
+        if key == pygame.K_d:
+            self.player.is_walking_right = False
+    def process_key_up_event(self, key):
+        if key == pygame.K_a:
+            self.player.is_walking_left = False
+        if key == pygame.K_d:
+            self.player.is_walking_right = False
+    def update_objects(self):
+        self.player.update()
+
+
+
+
+
     def render_objects(self, scena):
         scena.blit(self.background, (0, 0))
+        for platform in self.platforms:
+            platform.render(scena)
+        self.player.render(scena)
